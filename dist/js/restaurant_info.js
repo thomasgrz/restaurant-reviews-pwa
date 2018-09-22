ac40162dd392e7f558b9f25b1419d6b4740b02ab
thomas <thomasgrz@outlook.com>
2018-09-21 21:08:19 -0400

    updated accessibility to >93 score

--- dist/js/restaurant_info.js
+++ dist/js/restaurant_info.js
@@ -55,7 +55,7 @@ initMap = () => {
  * Get current restaurant from page URL.
  */
 fetchRestaurantFromURL = (callback) => {
-  
+  debugger
   if (self.restaurant) { // restaurant already fetched!
     callback(null, self.restaurant)
     return;
